e362988399fc41f36f367227e86602653dd6c35b
Paulo Ricardo <1981928paulo@gmail.com>
2022-11-02 18:29:39 -0300

    NLW MOBILE

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,8 @@
+import { NativeBaseProvider, Center, Text} from 'native-base';
+export function SignIn(){
+    return(<Center flex={1} bgColor="black">
+        <Text color="white" fontSize={24}>Sign In</Text>
+
+      </Center>)
+    
+}
