36237a52dd99b69f65fce6180b73edf78a9aacc2
Paulo Ricardo <1981928paulo@gmail.com>
2022-11-04 18:30:37 -0300

    Aula 03 - Layout e estrutura visual mobile

--- App.tsx
+++ App.tsx
@@ -5,22 +5,27 @@ import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/Signin';
 import {THEME} from './src/styles/theme';
 
+import { AuthContextProvider } from './src/contexts/AuthContext';
+import { Pools } from './src/screens/Pools';
+
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold})
 
   return (
-    <NativeBaseProvider theme={THEME}>
-        <StatusBar
-          barStyle='light-content'
-          backgroundColor="transparent"
-          translucent
-        />
-        {
-          fontsLoaded?<SignIn/>:<Loading/>
-        }
-       
+    <AuthContextProvider>
+      <NativeBaseProvider theme={THEME}>
+          <StatusBar
+            barStyle='light-content'
+            backgroundColor="transparent"
+            translucent
+          />
+          {
+            fontsLoaded?<Pools/>:<Loading/>
+          }
+        
 
-    </NativeBaseProvider>
+      </NativeBaseProvider>
+    </AuthContextProvider>
   );
 }
 
